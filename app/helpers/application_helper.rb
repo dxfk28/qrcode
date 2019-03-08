ea464785de0bb3fa2b7fdbd7fdc05f97c2e227ae
贾雨蒙 <jiayumeng10722@bsdd.konicaminolta.jp>
2019-03-08 16:56:37 +0800

    添加主题

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1515,7 +1515,7 @@ module ApplicationHelper
 
   # Returns the path to the favicon
   def favicon_path
-    icon = (current_theme && current_theme.favicon?) ? current_theme.favicon_path : '/favicon.ico'
+    icon = (current_theme && current_theme.favicon?) ? current_theme.favicon_path : '/images/logo2.png'
     image_path(icon)
   end
 
